d5df35bcfa67b6ce0f2b4abce2ff9e5493e17287
HighestDev <kenneth.danso@amalitech.org>
2023-05-21 12:00:10 +0000

    using postgres as our backend

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +80,13 @@ WSGI_APPLICATION = "bookmarks.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER":os.environ.get("DATABASE_USERNAME"),
+        "PASSWORD":os.environ.get("DATABASE_PASSWORD"),
+        "HOST":os.environ.get("DATABASE_HOST"),
+        "PORT":os.environ.get("DATABASE_PORT")
+
     }
 }
 
--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('',TemplateView.as_view(template_name='accounts/index.html')),
+    path('',TemplateView.as_view(template_name='index.html')),
     path("admin/", admin.site.urls),
-    path('',include('accounts.urls',namespace='accounts')),
+    path('account/',include('accounts.urls',namespace='accounts')),
 ]
