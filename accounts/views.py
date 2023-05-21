f36ab7b73936d325c7b7d0ed6673661d55ca3214
HighestDev <kenneth.danso@amalitech.org>
2023-05-21 21:15:24 +0000

    creating forms to allow visitors log into our application: register.html and connecting it up with views and finalizing it in the urls.

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from .forms import LoginForm
+from django.shortcuts import render, redirect
+from .forms import LoginForm, UserRegistrationForm
 from django.http import HttpResponse
 
 def user_login(request):
@@ -17,7 +17,7 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request,user)
-                    return HttpResponse('authenticated')
+                    return redirect('/accounts/dashboard')
                 else:
                     return HttpResponse('Disabled accounts')
             else:
@@ -27,6 +27,26 @@ def user_login(request):
     context={'form':form}
     return render(request,'accounts/login.html',context)
 
-@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('/accounts/login/')
+
+def registerUser(request):
+    if request.method=="POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user=user_form.cleaned_data
+            new_user.save(commit=False)
+
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            new_user.save()
+            return render(request, 'accounts/register_done.html',{'new_user':new_user})
+    else:
+        user_form=UserRegistrationForm()
+    return render(request,'accounts/register.html',{'user_form':user_form})
+
+@login_required(login_url='/accounts/login/')
 def dashboard(request):
     return render(request,'accounts/dashboard.html',{'section':'dashboard'})
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -133,3 +133,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # LOGIN_REDIRECT_URL = 'dashboard'
 # LOGIN_URL = 'login'
 # LOGOUT_URL = 'logout'
+
+
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
