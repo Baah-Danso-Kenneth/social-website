c6200158fea187a84e131559f08f1798d2a0ed8c
HighestDev <kenneth.danso@amalitech.org>
2023-05-21 14:34:33 +0000

    login logic implemented in views and urls without using the django auth approach

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
+from .forms import LoginForm
+from django.http import HttpResponse
 
 # Create your views here.
+def user_login(request):
+    if request.method=='POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            username=cd['username']
+            password=cd['password']
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return HttpResponse('authenticated')
+                else:
+                    return HttpResponse('Disabled accounts')
+            else:
+                return HttpResponse('Invalid Credentials')
+    else:
+        form=LoginForm()
+    context={'form':form}
+    return render(request,'accounts/login.html',context)
+
+
+
