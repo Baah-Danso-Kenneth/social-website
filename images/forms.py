e8062c34293ede2c2f24d260c4c91d92d0e10fbb
HighestDev <kenneth.danso@amalitech.org>
2023-05-23 11:44:35 +0000

    overriden the save of the model so that users can save image using the url

--- images/forms.py
+++ images/forms.py
@@ -1,4 +1,7 @@
+import requests
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from .models import Image
 
 class ImageCreationForm(forms.ModelForm):
@@ -16,3 +19,19 @@ class ImageCreationForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError('The given url does not match the valid image extensions')
         return url
+
+
+    def save(self, force_insert=False, commit=True):
+        image = super().save(commit=False)
+        image_url =self.cleaned_data['url']
+        name = slugify(image.title)
+        extension= image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+
+        if commit:
+            image.save()
+        return image
+
